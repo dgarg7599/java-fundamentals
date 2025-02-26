941cca2bbb955d88b5514652a42e0b6229ca8f21
dgarg7599 <gargdivyansh7599@gmail.com>
2025-02-26 17:17:57 +0530

    Printing a message.

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,5 +1,5 @@
 public class FirstJavaProgram{
 	public static void main(String[] args){
-		System.out.print("Capgemini training started by bridgelabz.");
+		System.out.println("Capgemini training started by bridgelabz.");
 	}
 }
